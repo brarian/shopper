abe059c6782d212882bbabf5f467308e563943f9
Brarian <b.namonywa@gmail.com>
2019-05-19 00:15:31 -0500

    authentication comments

---
+++ middleware/authentication.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+// need to set up db first to get data from?
+
+class Authenticate {
+  static authenticateUser(){
+    //find user 
+
+    //==>find user  error
+
+    //check pw match 
+
+    //==> pw match error 
+  }
+
+  //if user and pw is right send token
+  static generateToken(){
+
+  }
+}
